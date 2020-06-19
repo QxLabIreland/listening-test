c707ffec70f30c1afa557b6b43e0f48471f49dea
MirageJian <zhang745426@outlook.com>
2020-06-19 16:07:27 +0100

    Sign-in, Sing-up and Password change APIs

---
+++ listening-test-server/handlers/auth/login.py
@@ -0,0 +1,40 @@
+from handlers.base import BaseHandler
+
+
+class LoginHandler(BaseHandler):
+    async def get(self):
+        # Request the _xsrf cookies interface.
+        if self.xsrf_token:
+            # Check user logged or not
+            if self.get_current_user():
+                # Get user information
+                user = self.db['users'].find_one({'_id': self.get_current_user()})
+                del user['password']
+                self.dumps_write(user)
+            else:
+                self.set_status(200, 'No user login')
+        else:
+            self.set_status(200, reason="No xsrf token, set csrf token successfully")
+
+    async def post(self):
+        body = self.loads_body()
+        user = self.db['users'].find_one({'email': body["email"]})
+        if not user:
+            self.set_status(404, reason="User not found")
+            return
+
+        # hashed_password = yield executor.submit(
+        #     bcrypt.hashpw, tornado.escape.utf8(self.get_argument("password")),
+        #     tornado.escape.utf8(author.hashed_password))
+
+        if str(body["password"]) == str(user["password"]):
+            # Set_secure_cookie, which cannot be get by browser
+            self.set_secure_cookie("_user", str(user["_id"]), 3, httponly=True, secure=False)
+            del user['password']
+            self.dumps_write(user)
+            # self.redirect(self.get_argument("next", "/"))
+        else:
+            self.set_status(401, reason="Incorrect password")
+
+    async def delete(self):
+        self.clear_cookie("_user")
