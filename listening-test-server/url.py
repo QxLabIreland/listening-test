2a437d0c30cb1c6320c51615a965f7f220ce8183
MirageJian <zhang745426@outlook.com>
2020-06-23 15:27:09 +0100

    Server code cleaning.

--- listening-test-server/url.py
+++ listening-test-server/url.py
@@ -1,14 +1,3 @@
-from handlers.pre_delete.homepage import Homepage
-from handlers.pre_delete.something import SomethingHandler
-from handlers.pre_delete.about import AboutHandler
-from handlers.pre_delete.article import ArticleHandler
-from handlers.pre_delete.comment import CommentHandler
-from handlers.pre_delete.comment_managing import CommentManagingHandler
-from handlers.pre_delete.category import CategoryHandler
-from handlers.pre_delete.searching import SearchingHandler
-from handlers.files_management.public_files import PublicFilesHandler
-
-
 from handlers.auth.sign_up_handler import SignUpHandler
 from handlers.auth.login import LoginHandler
 from handlers.auth.password import PasswordHandler
@@ -37,15 +26,5 @@ path = [
 
     # Survey and Task
     ("/api/task/ab-test", AbTestSurveyHandler),
-
-    (api_url + r"/dashboard", Homepage),
-    (api_url + r"/something", SomethingHandler),
-    (api_url + r"/about", AboutHandler),
-    (api_url + r"/article", ArticleHandler),
-    (api_url + r"/comment", CommentHandler),
-    (api_url + r"/comment_managing", CommentManagingHandler),
-    (api_url + r"/category", CategoryHandler),
-    (api_url + r"/searching", SearchingHandler),
-    (api_url + r"/public_files", PublicFilesHandler),
 ]
 
