15a699b7b9445fde58d1cc0fedaef185f9053037
MirageJian <zhang745426@outlook.com>
2020-08-17 18:27:02 +0100

    READEME.md updated, database name changed.

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 echo "mongodb-org hold" | sudo dpkg --set-selections
