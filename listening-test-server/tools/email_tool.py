7decf8d9da2cf804d0b309c36faf168823573765
MirageJian <zhang745426@outlook.com>
2020-09-04 21:50:11 +0100

    Send email with From and To set without header for From.

--- listening-test-server/tools/email_tool.py
+++ listening-test-server/tools/email_tool.py
@@ -1,5 +1,7 @@
 import subprocess
 from datetime import datetime
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
 from uuid import uuid4
 from pymongo.collection import Collection
 
@@ -11,15 +13,36 @@ def send_confirmation_email(col: Collection, email: str):
         'createdAt': datetime.now(), 'confirmationCode': confirmation_code
     }})
     # Send activation email with linux current user (If you don't have a MTA you can disable it)
-    message = f"https://golisten.ucd.ie/confirm-email?{confirmation_code}\t" \
-              f"The confirmation link will be invalid in 30 minutes"
+    message = f"https://golisten.ucd.ie/confirm-email?{confirmation_code}\r\n" \
+              f"Click the link to confirm your email address and it will be invalid in 30 minutes"
     subject = "Please confirm your account"
-    subprocess.run(f'echo "{message}" | mail -s "{subject}" {email}', shell=True)
+    sender = 'noreply@golisten.ucd.ie'
+    subprocess.run(f'echo "Content-Type: text/plain\r\nFrom: {sender}\r\nTo: {email}\r\n'
+                   f'Subject: {subject}\r\n\r\n{message}" | sendmail -f {sender} {email}', shell=True)
 
 
 def send_password_reset_email(confirmation: str, email: str):
     # Send email with linux current user
-    message = f"https://golisten.ucd.ie/find-password?{confirmation}\t" \
-              f"You are resetting the password and the link will be invalid in 30 minutes\t"
-    subject = "Please reset your password"
-    subprocess.run(f'echo "{message}" | mail -s "{subject}" {email}', shell=True)
+    message = f"https://golisten.ucd.ie/find-password?{confirmation}\r\n" \
+              f"You are resetting the password and the link will be invalid in 30 minutes"
+    subject = "Please check your reset password link"
+    sender = 'noreply@golisten.ucd.ie'
+    subprocess.run(f'echo "Content-Type: text/plain\r\nFrom: {sender}\r\nTo: {email}\r\n'
+                   f'Subject: {subject}\r\n\r\n{message}" | sendmail -f {sender} {email}', shell=True)
+
+
+def send_password_reset_email2(confirmation: str, target_email: str):
+    # Create an email content
+    msg = MIMEText(f"https://golisten.ucd.ie/find-password?{confirmation}\r\n"
+                   f"You are resetting the password and the link will be invalid in 30 minutes\r\n")
+    msg['Subject'] = 'Please reset your password'
+    msg['From'] = 'noreply@golisten.ucd.ie'
+    msg['To'] = target_email
+    # Connect to localhost email server
+    conn = SMTP_SSL('localhost')
+    try:
+        # Login and send email
+        conn.sendmail('noreply@golisten.ucd.ie', target_email, msg.as_string())
+    finally:
+        conn.quit()
+    # subprocess.run(f'echo "{message}" | mail -s "{subject}" {email}', shell=True)
