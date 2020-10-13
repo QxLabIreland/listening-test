904153bcc821815dbba0ff12a680ed9101d2c3a5
MirageJian <zhang745426@outlook.com>
2020-10-13 17:28:12 +0100

    Storage status updated. Image and video now will be calculated into size.

--- listening-test-server/handlers/miscellanea/storage_status.py
+++ listening-test-server/handlers/miscellanea/storage_status.py
@@ -1,58 +1,50 @@
 import os
+from collections import namedtuple
+
 from pymongo.database import Database
 from handlers.base import BaseHandler
 from tools.file_helper import sizeof_fmt
 
 
 class StorageStatusHandler(BaseHandler):
+    storage_folder = ['static2/audio_files', 'static2/audio_files', 'static2/videoFile']
+
     async def prepare(self):
         # Get user and check the permissions
         self.user_id = await self.auth_current_user('Storage')
 
     async def get(self):
         medias_checklist = get_medias_in_using(self.db)
-        # TODO try to categorize video, audio and image
-        target_path = 'static2/audio_files'
+        storage_status_dto = {'totalSize': 0, 'totalNum': 0, 'redundantSize': 0}
         # Get size of static folder
-        total_size = 0
-        total_num = 0
-        redundant_size = 0
-        if not os.path.exists(target_path):
-            return
-        # Walk the target direction
-        for dirpath, dirnames, filenames in os.walk(target_path):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                # skip if it is symbolic link
-                if not os.path.islink(fp):
-                    f_size = os.path.getsize(fp)
-                    total_size += f_size
-                    total_num += 1
-                    # If a file is not in use, mark it as redundant
-                    if f not in medias_checklist:
-                        redundant_size += f_size
+        for target_path in StorageStatusHandler.storage_folder:
+            path_usage = get_space_usage(target_path, medias_checklist)
+            if path_usage:
+                for key, value in path_usage.items():
+                    storage_status_dto[key] += value
 
         self.dumps_write({
-            'totalSize': sizeof_fmt(total_size),
-            'totalNum': total_num,
-            'redundantSize': sizeof_fmt(redundant_size)
+            'totalSize': sizeof_fmt(storage_status_dto['totalSize']),
+            'totalNum': storage_status_dto['totalNum'],
+            'redundantSize': sizeof_fmt(storage_status_dto['redundantSize'])
         })
 
     async def delete(self):
         medias_checklist = get_medias_in_using(self.db)
-        target_path = 'static2/audio_files'
         total_size = 0
         total_num = 0
-        if not os.path.exists(target_path):
-            return
-        for f in os.listdir(target_path):
-            # Check if file has been used, if it has then delete it
-            if f not in medias_checklist:
-                os.remove(os.path.join(target_path, f))
-            else:
-                # Add size and increase number
-                total_size += os.path.getsize(os.path.join(target_path, f))
-                total_num += 1
+
+        for target_path in StorageStatusHandler.storage_folder:
+            if not os.path.exists(target_path):
+                continue
+            for f in os.listdir(target_path):
+                # Check if file has been used, if it has then delete it
+                if f not in medias_checklist:
+                    os.remove(os.path.join(target_path, f))
+                else:
+                    # Add size and increase number
+                    total_size += os.path.getsize(os.path.join(target_path, f))
+                    total_num += 1
 
         self.dumps_write({
             'totalSize': sizeof_fmt(total_size),
@@ -65,12 +57,38 @@ def get_medias_in_using(db: Database) -> set:
     medias_checklist = set()
     # Get all collections and map data
     for col in db.list_collection_names():
-        data = db[col].find({}, {'items.example.medias.src': 1})
+        data = db[col].aggregate([
+            {"$addFields": {"listSrc": {'$reduce': {
+                'input': "$items.example.medias",
+                'initialValue': [],
+                'in': {'$concatArrays': ["$$value", "$$this.src"]}
+            }}}},
+            {'$project': {'listSrc': 1}},
+            {'$match': {'listSrc.0': {'$exists': True}}}
+        ])
         for d in data:
-            if 'items' in d and d['items']:
-                for i in d['items']:
-                    if 'example' in i and i['example'] and 'medias' in i['example']:
-                        for a in i['example']['medias']:
-                            if 'src' in a and a['src']:
-                                medias_checklist.add(os.path.split(a['src'])[-1])
+            for src in d['listSrc']:
+                medias_checklist.add(os.path.split(src)[-1])
     return medias_checklist
+
+
+def get_space_usage(target_path, db_medias_checklist):
+    size = 0
+    count = 0
+    redundant_size = 0
+    if not os.path.exists(target_path):
+        return None
+    # Walk the target direction
+    for dirpath, dirnames, filenames in os.walk(target_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                f_size = os.path.getsize(fp)
+                size += f_size
+                count += 1
+                # If a file is not in use, mark it as redundant
+                if f not in db_medias_checklist:
+                    redundant_size += f_size
+
+    return {'totalSize': size, 'totalNum': count, 'redundantSize': redundant_size}
--- listening-test-server/tests/mongo_query_medias.py
+++ listening-test-server/tests/mongo_query_medias.py
@@ -1,22 +1,33 @@
 import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-# db = client["golisten_db"]
-db = client["listeningTestDb"]
+db = client["golisten_db"]
+# db = client["listeningTestDb"]
 
 medias = set()
 
 for col in db.list_collection_names():
 
-    data = db[col].find({}, {'items.example.medias.src': 1})
-
+    data = db[col].aggregate([
+        {"$addFields": {"list": {'$reduce': {
+            'input': "$items.example.medias",
+            'initialValue': [],
+            'in': {'$concatArrays': ["$$value", "$$this.src"]}
+        }}}},
+        {'$project': {'list': 1}},
+        {'$match': {'list.0': {'$exists': True}}}
+    ])
+    print(col)
     for d in data:
         print(d)
-        if 'items' in d and d['items']:
-            for i in d['items']:
-                if 'example' in i and i['example'] and 'medias' in i['example']:
-                    for a in i['example']['medias']:
 
-                        medias.add(a['src'])
+    # for d in data:
+    #     print(d)
+    #     if 'items' in d and d['items']:
+    #         for i in d['items']:
+    #             if 'example' in i and i['example'] and 'medias' in i['example']:
+    #                 for a in i['example']['medias']:
+    #
+    #                     medias.add(a['src'])
 
 print(medias, len(medias))
