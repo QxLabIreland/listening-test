e2f17ace18f6a0f9ddcaac531b48a95f33c357b5
MirageJian <zhang745426@outlook.com>
2020-12-01 23:59:27 +0000

    Sing up prompt for finish page

--- listening-test-react/src/views/shared-views/SurveyFinishPage.tsx
+++ listening-test-react/src/views/shared-views/SurveyFinishPage.tsx
@@ -6,11 +6,12 @@ import Axios from "axios";
 import {useSimpleAlert} from "../../shared/components/UseSimpleAlert";
 import {useLocation} from "react-router";
 import {getCurrentHost} from "../../shared/UncategorizedTools";
+import {Link as RouterLink} from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   paragraphCenter: {marginBottom: theme.spacing(4), textAlign: 'center'},
   paragraph: {marginBottom: theme.spacing(4)},
-  actions: {textAlign: 'center', marginTop: theme.spacing(4)}
+  actions: {textAlign: 'center', marginTop: theme.spacing(4)},
 }))
 
 export default function SurveyFinishPage() {
@@ -37,11 +38,17 @@ export default function SurveyFinishPage() {
       <Typography variant="h5" className={classes.paragraph}>
         Thank you for taking part in this test
       </Typography>
-
-      {location.state ? <Typography variant="body2" color="textSecondary" className={classes.paragraphCenter}>
-        Your responses are anonymous but if you do wish delete your responses at a later time, please save and visit
-        this link: <Link href={deletionLink} target="_blank">{deletionLink}</Link>
-      </Typography> : <>
+      {/*Survey finished page / Delete response page*/}
+      {location.state ? <>
+        <Typography variant="body2" color="textSecondary" className={classes.paragraphCenter}>
+          Your responses are anonymous but if you do wish delete your responses at a later time, please save and visit
+          this link: <Link href={deletionLink} target="_blank">{deletionLink}</Link>
+        </Typography>
+        <Typography variant="body1" className={classes.paragraphCenter}>
+          If you would like to create and share your own tests.
+        </Typography>
+        <Button variant="contained" color="primary" component={RouterLink} to="/sign-up" target="_blank">Go sign up!</Button>
+      </> : <>
         <Typography variant="body2" className={classes.paragraph}>
           Are you sure you want to delete your responses {id}?. Your responses are anonymous but if you do
           wish delete your responses please click CONFIRM button.
