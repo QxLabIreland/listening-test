170fde899bbca4a35635c1ba8618f8a2fbe33a70
MirageJian <zhang745426@outlook.com>
2021-04-12 12:24:41 +0100

    Fix pack_transfer

--- pack_transfer.py
+++ pack_transfer.py
@@ -48,12 +48,12 @@ try:
 
     # You may need to delete this
     print('Transfer the files to server...')
-    subprocess.run(f'scp -i golisten_ssh_key.pem -r {base_dir}/* golistenadmin@golisten.ucd.ie:/home/golistenadmin/golisten/')
+    subprocess.run(f'scp -i golisten_ssh_key.pem -r {base_dir}/* golistenadmin@golisten.ucd.ie:/home/golistenadmin/golisten/', shell=True)
 
     print('Start ssh and run script...')
     # subprocess.run('ssh -i ForStudy.pem ubuntu@63.34.10.16 "pip install tornado pymongo"')
     # subprocess.run('ssh -i ForStudy.pem ubuntu@63.34.10.16 "sudo supervisorctl reload"')
-    subprocess.run('ssh -i golisten_ssh_key.pem -t golistenadmin@golisten.ucd.ie "sudo supervisorctl restart golisten"')
+    subprocess.run('ssh -i golisten_ssh_key.pem -t golistenadmin@golisten.ucd.ie "sudo supervisorctl restart golisten"', shell=True)
     input('You can close the window now')
 
 except Exception as ex:
