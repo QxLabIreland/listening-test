ecaf33d0bab00bd3e4cf00c7097b2d703bfc80f4
MirageJian <zhang745426@outlook.com>
2022-04-14 22:36:14 +0100

    prettier and misc updates

--- listening-test-react/src/components/audio/AbTest/AbAddItemButtonGroup.tsx
+++ listening-test-react/src/components/audio/AbTest/AbAddItemButtonGroup.tsx
@@ -1,15 +1,17 @@
-import {observer} from "mobx-react";
-import {AudioTestItemModel} from "../../../shared/models/AudioTestModel";
-import {SurveyControlType, TestItemType} from "../../../shared/models/EnumsAndTypes";
-import {uuid} from "uuidv4";
-import {Box, ListItemIcon, ListItemText, MenuItem} from "@material-ui/core";
-import Icon from "@material-ui/core/Icon";
-import React, {useRef} from "react";
-import {AddQuestionButton, AddQuestionButtonType} from "../../utils/AddQuestionButton";
-import {useMatStyles} from "../../../shared/SharedStyles";
+import { observer } from 'mobx-react';
+import React, { useRef } from 'react';
+import { uuid } from 'uuidv4';
+
+import { Box, ListItemIcon, ListItemText, MenuItem } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
+
+import { useMatStyles } from '../../../shared/SharedStyles';
+import { AudioTestItemModel } from '../../../shared/models/AudioTestModel';
+import { SurveyControlType, TestItemType } from '../../../shared/models/EnumsAndTypes';
+import { AddQuestionButton, AddQuestionButtonType } from '../../utils/AddQuestionButton';
 
 export const AbAddItemButtonGroup = observer(function (props: { onAdd: (type: AudioTestItemModel) => void }) {
-  const {onAdd} = props;
+  const { onAdd } = props;
   const classes = useMatStyles();
   const addQuestionMenu = useRef<AddQuestionButtonType>();
 
@@ -20,46 +22,57 @@ export const AbAddItemButtonGroup = observer(function (props: { onAdd: (type: Au
       type: TestItemType.example,
       title: 'Title (click to edit)',
       example: {
-        fields: [{
-          type: SurveyControlType.radio,
-          question: 'Select your preference from the options below',
-          options: ['A', 'B'],
-          value: null,
-          required: true
-        }, {type: SurveyControlType.text, question: 'Briefly comment on your choice.', value: null, required: false}],
-        medias: [null, null]
-      }
+        fields: [
+          {
+            type: SurveyControlType.radio,
+            question: 'Select your preference from the options below',
+            options: ['A', 'B'],
+            value: null,
+            required: true,
+          },
+          { type: SurveyControlType.text, question: 'Briefly comment on your choice.', value: null, required: false },
+        ],
+        medias: [null, null],
+      },
     });
-  }
+  };
 
   const handleAddTraining = () => {
     addQuestionMenu.current?.closeMenu();
     onAdd({
-      id: uuid(), type: TestItemType.training, title: 'Title (click to edit)', example: {
-        fields: [
-          {type: SurveyControlType.description, question: 'Please listen these sounds.', value: null}
-        ], medias: []
-      }
+      id: uuid(),
+      type: TestItemType.training,
+      title: 'Title (click to edit)',
+      example: {
+        fields: [{ type: SurveyControlType.description, question: 'Please listen these sounds.', value: null }],
+        medias: [],
+      },
     });
-  }
+  };
 
-  return <Box className={classes.elementGroup}>
-    {/*<Button variant="outlined" color="primary">
+  return (
+    <Box className={classes.elementGroup}>
+      {/*<Button variant="outlined" color="primary">
       <Icon>add</Icon>Add Section
     </Button>*/}
-    <AddQuestionButton ref={addQuestionMenu} onAdd={onAdd}>
-      <MenuItem onClick={handleAddExample}>
-        <ListItemIcon>
-          <Icon fontSize="small" style={{color: 'dodgerblue'}}>add_task</Icon>
-        </ListItemIcon>
-        <ListItemText>Audio AB Test</ListItemText>
-      </MenuItem>
-      <MenuItem onClick={handleAddTraining}>
-        <ListItemIcon>
-          <Icon fontSize="small" style={{color: 'coral'}}>fitness_center</Icon>
-        </ListItemIcon>
-        <ListItemText>Audio Training Example</ListItemText>
-      </MenuItem>
-    </AddQuestionButton>
-  </Box>
+      <AddQuestionButton ref={addQuestionMenu} onAdd={onAdd}>
+        <MenuItem onClick={handleAddExample} data-testid="buttonAddEx">
+          <ListItemIcon>
+            <Icon fontSize="small" style={{ color: 'dodgerblue' }}>
+              add_task
+            </Icon>
+          </ListItemIcon>
+          <ListItemText>Audio AB Test</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleAddTraining}>
+          <ListItemIcon>
+            <Icon fontSize="small" style={{ color: 'coral' }}>
+              fitness_center
+            </Icon>
+          </ListItemIcon>
+          <ListItemText>Audio Training Example</ListItemText>
+        </MenuItem>
+      </AddQuestionButton>
+    </Box>
+  );
 });
