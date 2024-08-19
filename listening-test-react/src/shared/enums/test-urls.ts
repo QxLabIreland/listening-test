ed7b9d4e091812ac575adce3d7f5344c3e1c3f23
MirageJian <zhang745426@outlook.com>
2024-08-18 22:09:43 -0400

    Update titles

--- listening-test-react/src/shared/enums/test-urls.ts
+++ listening-test-react/src/shared/enums/test-urls.ts
@@ -13,19 +13,21 @@ export const TEST_URLS = [
 // Types of string for kinds of test
 export type TestUrl = (typeof TEST_URLS)[number];
 
-export const URL_TO_TITLE: { [key: string]: string } = {
+export const URL_TO_TITLE = {
   dashboard: 'Go Listen Home',
-  storage: 'Sotrage Status',
+  storage: 'Storage Status',
   people: 'User Management',
   template: 'Template Management',
   settings: 'Settings',
-  'ab-test': 'AB Test',
-  'acr-test': 'ACR Test',
-  'mushra-test': 'MUSHRA Test',
-  'hearing-test': 'Hearing Sensitivity Test',
-  'audio-labeling': 'Audio Labeling Task',
-  'image-labeling': 'Image Labeling Task',
-  'image-ab': 'Image AB Task',
-  'video-labeling': 'Video Labeling Task',
-  'video-ab': 'Video AB Task',
+  'ab-test': 'My Audio AB Tests',
+  'acr-test': 'My Audio ACR Tests',
+  'mushra-test': 'My Audio MUSHRA Tests',
+  'hearing-test': 'My Hearing Sensitivity Tests',
+  'audio-labeling': 'My Audio Labeling Tasks',
+  'image-labeling': 'My Image Labeling Tasks',
+  'image-ab': 'My Image AB Task',
+  'video-labeling': 'My Video Labeling Task',
+  'video-ab': 'My Video AB Task',
 } as const;
+
+export const RESPONSE_HASH_URL = '#responses';
